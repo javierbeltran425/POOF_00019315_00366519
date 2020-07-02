8ae66b87480539cc875846f2aad525017217a6ad
Andrea-09 <54600387+Andrea-09@users.noreply.github.com>
2020-07-02 11:31:55 -0600

    Connection BD and Vigilante View

---
+++ ExamenFinalPOO/ConnectionBD.cs
@@ -0,0 +1,47 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenFinalPOO
+{
+    public static class ConnectionBD
+    {
+        private static string host = "127.0.0.1",
+            database = "ParcialFinal",
+            userId = "postgres",
+            password = "pimienta";
+
+        private static string sConnection = $"Server={host}; Port=5432; User id={userId}; " +
+                                            $"Password={password}; Database={database};";
+
+        public static DataTable ExecuteQuery(string query)
+        {
+            NpgsqlConnection connection = new NpgsqlConnection(sConnection);
+            DataSet ds = new DataSet();
+
+            connection.Open();
+
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(query, connection);
+            da.Fill(ds);
+
+            connection.Close();
+
+            return ds.Tables[0];
+        }
+
+        public static void ExecuteNonQuery(string act)
+        {
+            Console.WriteLine(sConnection);
+            NpgsqlConnection connection = new NpgsqlConnection(sConnection);
+
+            connection.Open();
+            NpgsqlCommand command = new NpgsqlCommand(act, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
