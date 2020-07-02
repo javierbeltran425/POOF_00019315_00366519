71351fe00d28ad928b6e3309374f053e9058656c
Andrea-09 <54600387+Andrea-09@users.noreply.github.com>
2020-07-02 10:03:08 -0600

    Add: classes Registro and Departamento

---
+++ ExamenFinalPOO/Departamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenFinalPOO
+{
+    class Departamento
+    {
+        public int IdDepartamento { get; set; }
+        public string Nombre { get; set; }
+        public string Ubicacion { get; set; }
+    }
+}
---
+++ ExamenFinalPOO/Registro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenFinalPOO
+{
+    class Registro
+    {
+        public int IdRegistro { get; set; }
+        public int IdUsuario { get; set; }
+        public bool Entrada { get; set; }
+        public double Temperatura { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
