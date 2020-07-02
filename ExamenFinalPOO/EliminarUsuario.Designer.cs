bb0b3888d17fa8a1f5f7c4073daec5c30e8e152d
Andrea-09 <54600387+Andrea-09@users.noreply.github.com>
2020-07-02 16:30:55 -0600

    Delete User query

--- ExamenFinalPOO/EliminarUsuario.Designer.cs
+++ ExamenFinalPOO/EliminarUsuario.Designer.cs
@@ -55,7 +55,7 @@
             // 
             this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(3, 193);
+            this.comboBox1.Location = new System.Drawing.Point(3, 195);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(460, 21);
             this.comboBox1.TabIndex = 0;
@@ -63,12 +63,13 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(469, 192);
+            this.button1.Location = new System.Drawing.Point(469, 194);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Eliminar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // EliminarUsuario
             // 
@@ -77,6 +78,7 @@
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "EliminarUsuario";
             this.Size = new System.Drawing.Size(622, 411);
+            this.Load += new System.EventHandler(this.EliminarUsuario_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
--- ExamenFinalPOO/EliminarUsuario.cs
+++ ExamenFinalPOO/EliminarUsuario.cs
@@ -16,5 +16,42 @@ namespace ExamenFinalPOO
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = $"SELECT idUsuario FROM USUARIO WHERE nombre = '{comboBox1.SelectedValue}'";
+
+                var dt = ConnectionBD.ExecuteQuery(query);
+                var dr = dt.Rows[0];
+                var idUsuario = Convert.ToInt32(dr[0].ToString());
+
+                string nonQuery = $"DELETE FROM USUARIO AND REGISTRO WHERE idUsuario = {idUsuario}";
+
+                ConnectionBD.ExecuteNonQuery(nonQuery);
+
+
+                MessageBox.Show("Se ha eliminado el usuario");
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ha ocurrido un error!");
+            }
+        }
+
+        private void EliminarUsuario_Load(object sender, EventArgs e)
+        {
+            var name = ConnectionBD.ExecuteQuery("SELECT idUsuario FROM USUARIO");
+            var nameCombo = new List<string>();
+
+            foreach (DataRow dr in name.Rows)
+            {
+                nameCombo.Add(dr[0].ToString());
+            }
+
+            comboBox1.DataSource = nameCombo;
+        }
     }
 }
